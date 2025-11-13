a34976ee3b310554dced8cf85770c14783ae431f
Roberto-Rgc <robertodegoescorreia@gmail.com>
2025-11-13 09:41:03 -0300

    Criado projeto com scroll view

---
+++ app/src/main/java/com/example/cardapioprojeto/TelaBebidasBackEnd.java
@@ -0,0 +1,14 @@
+package com.example.cardapioprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaBebidasBackEnd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_bebidas_front_end);
+    }
+}
